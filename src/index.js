c4c0b49271819384aede6e4e6df16faf72cfb3ef
n9delta <n9delta@mail.ru>
2022-09-08 23:17:05 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
 const { WebSocket } = require('ws'); require('dotenv').config();
 const echo = console.log;
 
-function normalizePayload(buffer) {
-	if (Object.prototype.toString.call(buffer).split('object ')[1] == 'Uint8Array]') {
-		return JSON.parse(String(buffer));
-	} else {
-		return buffer;
-	}
-}
-
-/*socket.on('message', function(data) {
-	data = normalizePayload(data);
-
-	if (data.op === 10) {
-		const jitter = Math.random();
-
-		setTimeout(function () {
-			let payload = { op: 1, d: null };
-			socket.send(JSON.stringify(payload));
-
-			console.log('begri')
-
-			setInterval(function () {
-				socket.send(payload);
-			}, data.d.heartbeat_interval);
-		}, data.d.heartbeat_interval * jitter);
-	}
-
-	if (data.op === 11) {
-		console.log(data);
-	}
-
-	console.log(data);
-});*/
-
 const EventEmitter = require('node:events');
 
 class Client extends EventEmitter {
